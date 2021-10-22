be2dd5ef04620aa796b93ffb9638435046839944
CoralieCC <coraliecharbonnay@gmail.com>
2021-10-22 15:56:08 +0200

    vide input

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
         //console.log(inputValue);
         addTask(inputValue);
         displayTasks();
+        inputValue = document.querySelector('#input').value = '';
     });
 })
 
