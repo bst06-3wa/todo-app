c871f30739b8e4b5f3f2dee1a8c16ff687fb31f3
Lepatron973 <djod973@live.fr>
2021-10-24 16:42:25 +0200

    ajout de la suppréssion de tâches

--- scripts/js/front.js
+++ scripts/js/front.js
@@ -4,12 +4,7 @@
 let app = document.querySelector('#app');
 let tasks = document.getElementsByTagName('li')
 let checkbox = document.getElementsByClassName('checkbox');
-//fonction permettant de trier les tầche dans un tableau
-//et de mettre celles qui sont validé en dernière position.
-function sortTask(){
-
-}
-//fonction permettant d'afficher les tâches triées.
+let delBtn = document.querySelector(".trash-content i")
 
 // fonction permettant de changer le statut d'une tâche
 // l'index et le status sont directement récupérés du DOM
@@ -23,6 +18,21 @@ function taskDone(){
     let storedTasks = localStorage.getItem('tasks');
     storedTasks = JSON.parse(storedTasks);
     storedTasks[index]['status'] = status;
+    localStorage.setItem('tasks',JSON.stringify(storedTasks));
+    displayTasks();
+}
+/* 
+    fonctionnant permmettant la suppréssion de tâches
+*/
+function delTask(){
+    /* 
+        l'index est placé sur la checkbox donc je suis obligé de remonter jusqu'à l'élément parent
+         le "li", pour redescendre jusqu'à la checbox et récupérer l'index
+    */
+    let index = parseInt(this.parentNode.parentNode.firstElementChild.firstElementChild.getAttribute('index'));
+    let storedTasks = localStorage.getItem('tasks');
+    storedTasks = JSON.parse(storedTasks);
+    storedTasks.pop(storedTasks[index]);
     localStorage.setItem('tasks',JSON.stringify(storedTasks));
     displayTasks();
 }
